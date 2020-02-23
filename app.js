1099ace9ed72b22046b35f6991f0b21d4e6e309b
CGTNathan <cgtnathan@gmail.com>
2020-02-23 13:43:28 -0500

    Updated npm modules

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(session({
 }));
 
 //Log connections to database
-const mongoMorgan = require('mongo-morgan');
+const mongoMorgan = require('mongoose-morgan');
 app.use(mongoMorgan(mongoDB, 'combined', {
   collection: 'logs'
 }));
